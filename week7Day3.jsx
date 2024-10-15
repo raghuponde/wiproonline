cf53626b9779230b2900863a03d29c1d5ada1cf0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-15 15:28:19 +0530

    seeprating file instrucror

--- week7Day3.jsx
+++ week7Day3.jsx
@@ -893,11 +893,54 @@ Remove All Students
 }
 export default CyclOPediaClassPage;
 
+    Now  i want to move instrctor details into seperate page and one value i will show 
+
+
+    add ome file Instructor.jsx and code for that 
+    ----------------------------------------------
+    
+import React from "react";
+class Instructor extends React.Component {
+constructor(props) {
+super(props);
+}
+componentDidUpdate() {
+console.log("Update - Instructor");
+}
+componentDidMount() {
+console.log("Mounted - Instructor");
+}
+componentWillUnmount() {
+console.log("UnMount - Instructor");
+
+}
+render() {
+console.log("Render - Instructor");
+return (
+<div className="p-3">
+<span className="h4 text-success">Instructor</span>
+<i className=" bi bi-toggle-off btn btn-success btn-sm"></i>
+<br />
+Name: {this.props.instructor.name} <br />
+Email : {this.props.instructor.email}
+<br />
+Phone : {this.props.instructor.phone}
+<br />
+</div>
+);
+}
+}
+export default Instructor;
 
 
 
+in cycloPediaclas page 
+    --------------------
+    in the retun after first div 
 
-
+    {this.state.instructor && (
+<Instructor instructor={this.state.instructor} />
+)}
 
     
 
