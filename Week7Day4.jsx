ea7f889198820cf41d5b15086ac7e5846026fcc9
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-16 09:48:14 +0530

    updated previous

--- Week7Day4.jsx
+++ Week7Day4.jsx
@@ -203,7 +203,42 @@ const Counter = () =>
 export default Counter;
 
 
+uppdated code again with previous 
+  __________________________________
+  import { useState } from "react";
 
+const Counter = () =>
+{
+    const [counter, setCounter] = useState(0);
+    
+    function incrementCounter()
+    {
+        setCounter(counter + 1);
+    }
+
+    function decrementCounter()
+    {
+        setCounter((prevState)=>{ return prevState -1});
+        setCounter((prevState)=>{ return prevState -1});
+    }
+
+        return (
+        <div className="col-12 col-md-4 offset-md-4 border text-white">
+        <span className="h2 pt-4 m-2 text-white-50">Fun Counter </span>
+        <br />
+        <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
+        <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
+        <br />
+        <span className="h4">
+        Counter: &nbsp;
+                    <span className="text-success">{counter }</span>
+        </span>
+        </div>
+            );
+    
+}
+
+export default Counter;
 
 
 
