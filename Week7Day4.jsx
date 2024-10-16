bcae4a5667c37e0e420c39604b5c0ff13eb5093b
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-16 09:27:51 +0530

    adding couter

--- Week7Day4.jsx
+++ Week7Day4.jsx
@@ -124,7 +124,44 @@ root.render(
 );
 
 
+Now add one file Counter.jsx in src folder and code for that 
 
+
+
+  const Counter = () => {
+return (
+<div className="col-12 col-md-4 offset-md-4 border text-white">
+<span className="h2 pt-4 m-2 text-white-50">Fun Counter </span>
+<br />
+<button className="btn btn-success m-1">+1</button>
+<button className="btn btn-danger m-1">-1</button>
+<br />
+<span className="h4">
+Counter: &nbsp;
+<span className="text-success">0</span>
+</span>
+</div>
+);
+}
+
+
+  and in index.jsx 
+
+  index.js
+*********
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter from "./Counter";
+import Header from "./Header";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+<React.StrictMode>
+<Header />
+<div className="p-2 m-2 row text-center">
+<Counter />
+</div>
+</React.StrictMode>
+);
 
 
 
