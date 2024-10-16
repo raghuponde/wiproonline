04cfaa722003ede7e63f0d970bff6ec5d4c15126
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-16 16:23:15 +0530

    updated page

--- Week7Day4.jsx
+++ Week7Day4.jsx
@@ -937,6 +937,134 @@ const CycloPediaFuncPage = () => {
   
 
 
+updated cyclofuncpage 
+  ---------------------
+      import React,{ useState } from "react";
+import { getRandomUser } from "./Utility/api";
+import Instructor from "./Instructor";
+const CycloPediaFuncPage = () => {
+    
+    const [state, setState] = useState(() => {
+         
+        return {
+           
+            instructor: undefined,
+            studentList: [],
+            studentCount: 0,
+            hideInstrcutor: false,
+
+        };
+
+
+    });
+    const [inputName, setInputName] = useState(() => {
+        return "";
+    })
+
+    const [inputFeedback, setInputFeedback] = useState(() => {
+        
+        return "";
+    })
+
+const handleAddStudent = () => {
+setState((prevState) => {
+    return {
+    ...prevState,
+studentCount: prevState.studentCount + 1,
+};
+});
+};
+const handleRemoveAllStudent = () => {
+        setState((prevState) => {
+            return {
+            ...prevState,
+        studentCount: 0,
+        };
+        });
+    };
+  const   handletoggleInstructor = () => {
+setState((prevState) => {
+    return {
+    ...prevState,
+hideInstructor: !prevState.hideInstructor,
+};
+});
+};
+
+
+return (
+    <div>
+         <div className="p-3">
+
+<i
+className={` bi ${
+state.hideInstructor ? "bi-toggle-off" : "bi-toggle-on"
+} btn btn-success btn-sm`}
+onClick={handletoggleInstructor}
+></i>
+{!state.hideInstructor && state.instructor ? (
+<Instructor instructor={state.instructor} />
+) : null}
+</div>
+    
+
+
+<div className="p-3">
+<span className="h4 text-success">Feedback</span>
+<br />
+<input
+type="text"
+value={inputName}
+placeholder="Name.."
+onChange={(e) => {
+setInputName(e.target.value);
+}}
+></input>{" "}
+Value : {inputName}
+<br />
+<textarea
+value={inputFeedback}
+onChange={(e) => {
+setInputFeedback(e.target.value);
+}}
+placeholder="Feedback..."
+></textarea>
+Value : {inputFeedback}
+</div>
+<div className="p-3">
+<span className="h4 text-success">Students</span> <br />
+<div>Student Count : {state.studentCount}</div>
+<button
+className="btn btn-success btn-sm"
+onClick={handleAddStudent}
+>
+Add Student
+</button>
+&nbsp;
+<button
+className="btn btn-danger btn-sm"
+onClick={handleRemoveAllStudent}
+>
+Remove All Students
+            </button>
+
+    {state.studentList.map((student, index) => {
+return (
+<div className="text-white" key={index}>
+- {student.name}
+</div>
+);
+})}
+
+</div>
+</div>
+);
+
+}
+export default CycloPediaFuncPage;
+
+
+
 
 
 
