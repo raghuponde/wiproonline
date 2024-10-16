d1698ea23ef479581252564190f83942872a986b
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-16 10:41:21 +0530

    Allways

--- Week7Day4.jsx
+++ Week7Day4.jsx
@@ -369,7 +369,51 @@ const Counter = () =>
 export default Counter;
 
 
+Allways of intilizeing shown here 
+  -------------------------------
+  import { useState } from "react";
+// function intialsetup()
+// {
+// console.log("hello");
+// return 10;
+// }
+const Counter = () =>
+{
+    const [counterState, setCounterState] = useState(() => { return { counter: 10,title2:"Fun" }; });
+    
+    const[titleState,setTitleState]=useState("Fun")// not much used 
+     const[titleState1,setTitleState1]=useState(() => { return { title1: "Fun" }; })
+    function incrementCounter()
+    {
+        setCounterState((prevState) => { return { counter:prevState.counter + 1 }; });
+    }
 
+    function decrementCounter()
+    {
+        setCounterState((prevState) => { return { counter: prevState.counter - 1 } });
+        setCounterState((prevState)=>{ return {counter:prevState.counter -1} });
+    }
+
+        return (
+        <div className="col-12 col-md-4 offset-md-4 border text-white">
+             
+                <span className="h2 pt-4 m-2 text-white-50">{titleState} Counter </span><br/>
+                <span className="h2 pt-4 m-2 text-white-50">{titleState1.title1} Counter </span><br/>
+                   <span className="h2 pt-4 m-2 text-white-50">{counterState.title2} Counter </span>
+        <br />
+        <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
+        <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
+        <br />
+        <span className="h4">
+        Counter: &nbsp;
+                    <span className="text-success">{counterState.counter }</span>
+        </span>
+        </div>
+            );
+    
+}
+
+export default Counter;
 
 
 
