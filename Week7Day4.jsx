eca9720cb4a25bb42f2893015cf27dc9adc072de
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-16 10:19:28 +0530

    finalcodeof appriate

--- Week7Day4.jsx
+++ Week7Day4.jsx
@@ -283,5 +283,61 @@ export default Counter;
 
   here constructor value has to be intilized only once whic is not happening 
 
+so here second time or third time hello should not come hello
+final code 
+  ------------
+import { useState } from "react";
+// function intialsetup()
+// {
+// console.log("hello");
+// return 10;
+// }
+const Counter = () =>
+{
+    const [counterState, setCounterState] = useState(() => { return { counter: 10 };});
+    
+    function incrementCounter()
+    {
+        setCounterState((prevState) => { return { counter:prevState.counter + 1 }; });
+    }
+
+    function decrementCounter()
+    {
+        setCounterState((prevState) => { return { counter: prevState.counter - 1 } });
+        setCounterState((prevState)=>{ return {counter:prevState.counter -1} });
+    }
+
+        return (
+        <div className="col-12 col-md-4 offset-md-4 border text-white">
+        <span className="h2 pt-4 m-2 text-white-50">Fun Counter </span>
+        <br />
+        <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
+        <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
+        <br />
+        <span className="h4">
+        Counter: &nbsp;
+                    <span className="text-success">{counterState.counter }</span>
+        </span>
+        </div>
+            );
+    
+}
+
+export default Counter;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
