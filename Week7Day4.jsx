54e0773172411f05c17a3ae2ce7b8fd96e1c9deb
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-16 09:57:31 +0530

    try this

--- Week7Day4.jsx
+++ Week7Day4.jsx
@@ -240,7 +240,48 @@ const Counter = () =>
 
 export default Counter;
 
+try this code once 
+  -------------------
+  import { useState } from "react";
+function intialsetup()
+{
+console.log("hello");
+return 10;
+}
+const Counter = () =>
+{
+    const [counter, setCounter] = useState(intialsetup());
+    
+    function incrementCounter()
+    {
+        setCounter(counter + 1);
+    }
 
+    function decrementCounter()
+    {
+        setCounter((prevState)=>{ return prevState -1});
+        setCounter((prevState)=>{ return prevState -1});
+    }
+
+        return (
+        <div className="col-12 col-md-4 offset-md-4 border text-white">
+        <span className="h2 pt-4 m-2 text-white-50">Fun Counter </span>
+        <br />
+        <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
+        <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
+        <br />
+        <span className="h4">
+        Counter: &nbsp;
+                    <span className="text-success">{counter }</span>
+        </span>
+        </div>
+            );
+    
+}
+
+export default Counter;
+
+  here constructor value has to be intilized only once whic is not happening 
 
 
 
