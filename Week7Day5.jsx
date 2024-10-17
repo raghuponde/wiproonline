a88290d8183ab5703d864e9931e94a0f53ec99e7
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 10:34:44 +0530

    routing 1t

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -429,7 +429,86 @@ write this command in command prompt means in terminal
 npm i react-router-dom@6.4.3
 
 
+index.js codde 
+------------------
+      import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+        <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+         <Route path="/about" element={<About/>}></Route>
 
+    </Routes>
+    </BrowserRouter>
+ </React.StrictMode>
+  
+);
+
+
+then in Hdeader
+      ----------
+import React from 'react'
+import logo from "./images/logo512.png"; 
+const Header = () => {
+    return (
+        <div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    
+                                
+            <img
+src={logo}
+alt=""
+style={{ height: "35px", verticalAlign: "top" }}
+/> &nbsp;
+                    
+                    Routing Demo</a>
+        
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+   
+  </div>
+            </nav>
+            </div>
+  )
+}
+
+export default Header
+      
 
       
 
