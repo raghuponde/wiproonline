571d1cb5ff884d3e52e42fc12a89aca26febee84
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 10:22:46 +0530

    header vesion1 chnage

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -367,8 +367,61 @@ root.render(
   
 );
 
+further modified Heaer 
+      ----------------
+      import React from 'react'
+import logo from "./images/logo512.png"; 
+const Header = () => {
+    return (
+        <div>
 
-      
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    
+                                
+            <img
+src={logo}
+alt=""
+style={{ height: "35px", verticalAlign: "top" }}
+/> &nbsp;
+                    
+                    Routing Demo</a>
+        
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+   
+  </div>
+            </nav>
+            </div>
+  )
+}
+
+export default Header
 
 
 
