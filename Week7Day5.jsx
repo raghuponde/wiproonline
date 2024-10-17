8f44d2584d967d120a47b4878f2b44d4bddb701b
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 10:58:10 +0530

    chnagemostly done

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -581,6 +581,119 @@ what are those four pages
       ProductDetails.jsx 
       ProductList.jsx 
 
-      
+      index.js file 
+      ----------------
+      import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import CreateProduct from "./Pages/CreateProduct";
+import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Pages/ProductDetails";
+import Product from "./Pages/Product";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+<React.StrictMode>
+<BrowserRouter>
+<Header />
+<Routes>
+<Route path="/" element={<Home />}></Route>
+<Route path="/about" element={<About />}></Route>
+<Route path="/product" element={<Product />}></Route>
+<Route path="/product/create" element={<CreateProduct />}></Route>
+<Route path="/product/details" element={<ProductDetails />}></Route>
+<Route path="/product/list" element={<ProductList />}></Route>
+</Routes>
+</BrowserRouter>
+</React.StrictMode>
+);
+
+      header.jsx 
+      ----------
+import React from "react";
+import logo from "./images/logo512.png";
+import { Link } from "react-router-dom";
+function Header() {
+return (
+<div>
+<nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+<div className="container-fluid">
+<img
+src={logo}
+alt=""
+style={{ height: "35px", verticalAlign: "top" }}
+/>
+<button
+className="navbar-toggler"
+type="button"
+data-bs-toggle="collapse"
+data-bs-target="#navbarSupportedContent"
+aria-controls="navbarSupportedContent"
+aria-expanded="false"
+aria-label="Toggle navigation"
+>
+<span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse"
+id="navbarSupportedContent">
+<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+<li className="nav-item">
+<Link className="nav-link active" aria-current="page" to="/">
+Home
+</Link>
+</li>
+<li className="nav-item">
+<Link className="nav-link" to="/about">
+About
+</Link>
+</li>
+<li className="nav-item dropdown">
+<a
+className="nav-link dropdown-toggle"
+href="#"
+role="button"
+data-bs-toggle="dropdown"
+aria-expanded="false"
+>
+Product
+</a>
+<ul className="dropdown-menu">
+<li>
+<Link className="dropdown-item" to="/product">
+Product
+</Link>
+</li>
+<li>
+<Link className="dropdown-item" to="/product/list">
+Product List
+</Link>
+</li>
+<li>
+<Link className="dropdown-item" to="/product/create">
+Product Create
+</Link>
+</li>
+<li>
+<Link className="dropdown-item" to="/product/details">
+Product Details
+</Link>
+</li>
+</ul>
+</li>
+<li className="nav-item">
+<a className="nav-link disabled">Disabled</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+</div>
+);
+}
+
+export default Header;
+
 
       
