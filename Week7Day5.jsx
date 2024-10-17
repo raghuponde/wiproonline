79648d92738494fd5cc98799c0700e2c2383cace
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 09:59:09 +0530

    adding pages

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -263,5 +263,55 @@ console.log("This will be called on when component will be UNMOUNTED");
 };
 }, []);
 
+Next new project is routing means how to provide routing to the application 
+create a new app in your regular base fodler with the name routingdemo 
+
+
+do the intial set up and after that in src folder i need Header.jsx ,About.jsx ,CryptoDetail.jsx,Home.jsx and NotFound.jsx 
+
+and index.html
+--------------
+      index.html
+***********
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="theme-color" content="#000000" />
+<meta name="description" content="Web site created using create-react-app"
+/>
+<link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+<link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+<link
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+rel="stylesheet"
+integrity="sha384-
+rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+crossorigin="anonymous" />
+<!-- JavaScript Bundle with Popper -->
+<script
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min
+.js"
+integrity="sha384-
+kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+crossorigin="anonymous"></script>
+<title>React App</title>
+</head>
+<body style="background-color: black; color: whitesmoke">
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<div id="root"></div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
 
 
