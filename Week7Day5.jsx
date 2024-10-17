95166326567bf9e0ae8332df238919b2f368c328
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 10:45:31 +0530

    hdeader 2 link usage

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -508,6 +508,71 @@ style={{ height: "35px", verticalAlign: "top" }}
 }
 
 export default Header
+
+whnever i am clciking the links the paeg is refreshsing so chnage the code like this use { Link }
+
+      Headder.jsx 
+      ---------------
+      import React from 'react'
+import logo from "./images/logo512.png"; 
+import { Link } from "react-router-dom";
+const Header = () => {
+    return (
+        <div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    
+                                
+            <img
+src={logo}
+alt=""
+style={{ height: "35px", verticalAlign: "top" }}
+/> &nbsp;
+                    
+                    Routing Demo</a>
+        
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      <Link className="nav-link active" aria-current="page" to="/">
+Home
+</Link>
+      </li>
+      <li class="nav-item">
+      <Link className="nav-link active" aria-current="page" to="/about">
+About
+</Link>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+   
+  </div>
+            </nav>
+            </div>
+  )
+}
+
+export default Header
+
+
       
 
       
