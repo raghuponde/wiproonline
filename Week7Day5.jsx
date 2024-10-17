f9f383db499e2246f16b202e97d93251f070a167
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 11:43:05 +0530

    Nesetd routes

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -694,6 +694,65 @@ Product Details
 }
 
 export default Header;
+Now i am doing nested routes and also if u type invalid route means * i will use here then notfound will be there 
+Index.js (Nested Routes) 
+---------------------------
+      import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import CreateProduct from "./Pages/CreateProduct";
+import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Pages/ProductDetails";
+import Product from "./Pages/Product";
+import NotFound from "./NotFound";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+<React.StrictMode>
+<BrowserRouter>
+<Header />
+<Routes>
+<Route path="/" element={<Home />}></Route>
+<Route path="/about" element={<About />}></Route>
+<Route path="product">
+<Route path="" element={<Product />}></Route>
+<Route path="create" element={<CreateProduct />}></Route>
+<Route path="details" element={<ProductDetails />}></Route>
+<Route path="list" element={<ProductList />}></Route>
+</Route>
+<Route path="*" element={<NotFound />}></Route>
+</Routes>
+</BrowserRouter>
+</React.StrictMode>
+);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
 
       
