583c185fcda934615773a97a8ed39e365f55dc96
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-17 12:20:33 +0530

    jumping wihtin the page to another page

--- Week7Day5.jsx
+++ Week7Day5.jsx
@@ -834,7 +834,30 @@ Product Details
 </Link>
 
 
+Now goto product.jsx file and add two buttons 
+-----------------------------------------------
 
+import { useNavigate, Link } from "react-router-dom";
+import React from "react";
+function Product() {
+const navigate = useNavigate();
+return (
+<div>
+Product
+<button
+onClick={() => {
+navigate("/product/create");
+}}
+>
+Add Product{" "}
+</button>
+<Link to={`/product/details/5`}>
+<button>Navigate to Product Detail - 5</button>
+</Link>
+</div>
+);
+}
+export default Product;
 
             
 
