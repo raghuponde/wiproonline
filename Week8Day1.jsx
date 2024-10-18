cbe209e2efa438ce3ecdf4f84f6eeb7edb0bcbdd
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-18 09:07:40 +0530

    Countercode

--- Week8Day1.jsx
+++ Week8Day1.jsx
@@ -1,1 +1,73 @@
-ss
+counterSlice.js 
+-------------------
+  import { createSlice } from "@reduxjs/toolkit";
+const initalState = { count: 10 };
+export const counterSlice = createSlice({
+name: "counter",
+initialState: initalState,
+reducers: {
+//here we will define all actions
+increment: (state) => {
+state.count += 1;
+},
+decrement: (state) => {
+state.count1 = 1;
+},
+},
+});
+export const { increment, decrement } = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
+
+store .js 
+----------
+  import { configureStore } from "@reduxjs/toolkit";
+import { counterReducer } from "./slice/counterSlice";
+
+export const store = configureStore({
+    reducer: {
+    counterStore: counterReducer,
+//we will fill this reducer in the upcoming videos
+},
+});
+
+// console.log(store.getState());
+// store.dispatch({
+// type: "counter/increment", });
+// console.log(store.getState());
+
+
+
+
+
+
+Counter.jsx code 
+-----------------
+  import React from "react";
+import { useSelector } from "react-redux";
+
+function Counter()
+{
+    const count = useSelector((state) => state.counterStore.count);
+    return <div>{count}</div>;
+}
+export default Counter;
+
+
+index.jsx 
+-------------
+  import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./app/layout/Header";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import Counter from "./app/components/Counter";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+<React.StrictMode>
+<Provider store={store}>
+            <Header />
+            <Counter />
+</Provider>
+</React.StrictMode>
+);
