2f979dbbfe218f4bad94eac63ae6aacbf0ec64f5
raghuponde <33595609+raghuponde@users.noreply.github.com>
2024-10-18 09:46:04 +0530

    added buttons

--- Week8Day1.jsx
+++ Week8Day1.jsx
@@ -75,3 +75,78 @@ root.render(
 </React.StrictMode>
 );
 
+Now i want buttons in Counter.jsx and want to use increment and decrement of counterSlice,js file 
+so updated counterSlice.js
+-----------------------------
+  import { createSlice } from "@reduxjs/toolkit";
+const initalState = { count: 10 };
+export const counterSlice = createSlice({
+name: "counter",
+initialState: initalState,
+reducers: {
+//here we will define all actions
+increment: (state) => {
+state.count += 1;
+},
+decrement: (state) => {
+state.count -= 1;
+},
+},
+});
+export const { increment, decrement } = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
+
+Counter.jsx 
+-------------
+  import React from "react";
+import { useSelector,useDispatch } from "react-redux";
+import { increment,decrement } from "../../redux/slice/counterSlice";
+function Counter() {
+    const count = useSelector((state) => state.counterStore.count);
+    const dispatch = useDispatch();
+    return (
+        
+        <div className="mt-2 pt-3 pl-2 text-center " style={{ borderTop:"1px solid #999"}}>
+            <div className="text-white pb-2 h4">Counter:{count}</div>
+            <div className="row">
+                <div className="p-4 col-12 col-md-6 ">
+                    <div className="border p-4">
+                        <button className="btn btn-primary" onClick={() => dispatch(increment())}>
+                            Add
+                        </button> {" "} &nbsp;
+                          <button className="btn btn-danger" onClick={() => dispatch(decrement())}>
+                            Remove
+                        </button>
+                   
+                </div>
+            </div>
+            </div>
+            </div>
+
+    )
+}
+export default Counter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
